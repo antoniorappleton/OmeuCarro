da8b2e48b5138c74c411da3d4081448688f32985
Seu Nome <seuemail@exemplo.com>
2025-12-10 19:10:23 +0000

    últimos ajustes

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,3 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("static-v1").then(cache => {
-      return cache.addAll([
-        "index.html",
-        "offline.html",
-        "css/style.css"
-      ]);
-    })
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match("offline.html"))
-  );
-});
-
 // service-worker.js
 
 const STATIC_CACHE = "l100-static-v1";
