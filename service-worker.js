8ba98f7bec018f8e7a3b9903d7c1d34056b1efb1
António Appleton <antonioappleton@gmail.com>
2026-02-11 18:13:10 +0000

    chore: bump app version to v76 and sw to v26

--- service-worker.js
+++ service-worker.js
@@ -3,15 +3,10 @@
 // ===============================
 
 // Aumenta a versão sempre que fizeres deploy
-<<<<<<< HEAD
-const STATIC_CACHE = "l100-static-v24";
-const RUNTIME_CACHE = "l100-runtime-v24";
-=======
+
 // Aumenta a versão sempre que fizeres deploy
-// Aumenta a versão sempre que fizeres deploy
-const STATIC_CACHE = "l100-static-v25";
-const RUNTIME_CACHE = "l100-runtime-v25";
->>>>>>> origin/main
+const STATIC_CACHE = "l100-static-v26";
+const RUNTIME_CACHE = "l100-runtime-v26";
 
 // Lista dos ficheiros essenciais para funcionar offline (APP SHELL)
 const APP_SHELL = [
@@ -80,11 +75,7 @@ self.addEventListener("install", (event) => {
 // ACTIVATE – Limpa caches antigas
 // ===============================
 self.addEventListener("activate", (event) => {
-<<<<<<< HEAD
-  console.log("[SW] Activate v23");
-=======
-  console.log("[SW] Activate v25");
->>>>>>> origin/main
+  console.log("[SW] Activate v26");
 
   event.waitUntil(
     caches.keys().then((names) =>
